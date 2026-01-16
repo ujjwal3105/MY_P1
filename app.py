284d341cfc0cf7ec208e2611feb34f26d88033b5
ujjwal3105 <152165252+ujjwal3105@users.noreply.github.com>
2026-01-16 16:15:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-# import numpy as np
-# import pandas as pd
-# import streamlit as st
-# import joblib
-
-# model=joblib.load("BEST.joblib")
-
-# st.title("HOUSE PRICE PREDECTION SYSTEM")
 import numpy as np 
 import pandas as pd 
 import streamlit as st 
